da5e3ad5d1b1878019d401ff644f74a60e55249c
claubermartins <clauber789@gmail.com>
2024-05-14 17:31:07 -0300

    Incluindo SurveyJS Angular e iniciando o component Quiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarAreaComponent } from './navbar-area/navbar-area.component';
 import { FormAreaComponent } from './form-area/form-area.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarAreaComponent,
-    FormAreaComponent
+    FormAreaComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
