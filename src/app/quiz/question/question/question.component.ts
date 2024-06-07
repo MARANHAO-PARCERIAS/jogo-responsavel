397d0a081e1711b9865795b481e768a50854f367
claubermartins <clauber789@gmail.com>
2024-06-07 16:41:20 -0300

    Corrige encerramento do quiz quando o tempo acaba na última questão

--- src/app/quiz/question/question/question.component.ts
+++ src/app/quiz/question/question/question.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { interval } from 'rxjs';
 import { QuestionService } from 'src/app/quiz/service/question.service';
 
-
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
@@ -20,80 +19,80 @@ export class QuestionComponent implements OnInit {
   inCorrectAnswer: number = 0;
   interval$: any;
   progress: string = "0";
-  isQuizCompleted : boolean = false;
-  constructor(private questionService: QuestionService, private router:Router) { }
+  isQuizCompleted: boolean = false;
+
+  constructor(private questionService: QuestionService, private router: Router) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
     this.getAllQuestions();
     this.startCounter();
   }
+
   getAllQuestions() {
     this.questionService.getQuestionJson()
       .subscribe(res => {
         this.questionList = res.questions;
-      })
+      });
   }
-  answer(currentQno: number, option: any) {
 
-    if(currentQno === this.questionList.length){
-      this.isQuizCompleted = true;
-      this.stopCounter();
-    }
+  answer(currentQno: number, option: any) {
     if (option.correct) {
       this.points += 10;
       this.correctAnswer++;
-      setTimeout(() => {
-        this.currentQuestion++;
-        this.resetCounter();
-        this.getProgressPercent();
-      }, 250);
-
-
     } else {
-      setTimeout(() => {
+      this.inCorrectAnswer++;
+    }
+    setTimeout(() => {
+      this.currentQuestion++;
+      this.resetCounter();
+      this.getProgressPercent();
+      if (this.currentQuestion === this.questionList.length) {
+        this.isQuizCompleted = true;
+        this.stopCounter();
+      }
+    }, 250);
+  }
+
+  startCounter() {
+    this.interval$ = interval(1000).subscribe(val => {
+      this.counter--;
+      if (this.counter === 0) {
         this.currentQuestion++;
-        this.inCorrectAnswer++;
         this.resetCounter();
         this.getProgressPercent();
-      }, 250);
-
-      // this.points -= 10;
-    }
-  }
-  startCounter() {
-    this.interval$ = interval(1000)
-      .subscribe(val => {
-        this.counter--;
-        if (this.counter === 0) {
-          this.currentQuestion++;
-          this.counter = 40;
-          // this.points -= 10;
+        if (this.currentQuestion === this.questionList.length) {
+          this.isQuizCompleted = true;
+          this.stopCounter();
         }
-      });
+      }
+    });
     setTimeout(() => {
       this.interval$.unsubscribe();
     }, 400000);
   }
+
   stopCounter() {
-    this.interval$.unsubscribe();
+    if (this.interval$) {
+      this.interval$.unsubscribe();
+    }
     this.counter = 0;
   }
+
   resetCounter() {
     this.stopCounter();
     this.counter = 40;
     this.startCounter();
   }
+
   getProgressPercent() {
     this.progress = ((this.currentQuestion / this.questionList.length) * 100).toString();
     return this.progress;
-
   }
-  sairQuiz(){
+
+  sairQuiz() {
     setTimeout(() => {
-      this.router.navigate(['/welcome'])
+      this.router.navigate(['/welcome']);
     }, 250);
-
   }
-
 }
