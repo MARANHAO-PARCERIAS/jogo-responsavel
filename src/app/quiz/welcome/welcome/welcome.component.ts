5d5e2d44691b6325430f8a02c2be3689757ac0be
claubermartins <clauber789@gmail.com>
2024-06-07 17:10:02 -0300

    Adiciona suporte para iniciar o quiz ao pressionar teclas de ação em dispositivos móveis.

--- src/app/quiz/welcome/welcome/welcome.component.ts
+++ src/app/quiz/welcome/welcome/welcome.component.ts
@@ -1,18 +1,30 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit{
+export class WelcomeComponent implements OnInit {
 
   @ViewChild('name') nameKey!: ElementRef;
-  constructor() { }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-  startQuiz(){
-    localStorage.setItem("name",this.nameKey.nativeElement.value);
+
+  startQuiz(): void {
+    const name = this.nameKey.nativeElement.value;
+    localStorage.setItem("name", name);
+    this.router.navigate(['/question']);
+  }
+
+  onKeyDown(event: KeyboardEvent): void {
+    const actionKeys = ['Enter', 'Go', 'Next', 'Done', 'Enviar', 'Concluir', 'OK', 'Pronto', 'Finalizar', 'Avançar'];
+    if (actionKeys.includes(event.key)) {
+      this.startQuiz();
+    }
   }
 }
